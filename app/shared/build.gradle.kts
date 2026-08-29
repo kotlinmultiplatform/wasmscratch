c5df9086880972637dc80d9980c757d79415b0c4
Dirk Hoffmann <hoffi.mail@gmail.com>
2026-08-29 22:55:26 +0200

    increased gradle.properties mem-settings

--- app/shared/build.gradle.kts
+++ app/shared/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 kotlin {
     listOf(
         iosArm64(),
-        iosSimulatorArm64()
+        iosSimulatorArm64(),
     ).forEach { iosTarget ->
         iosTarget.binaries.framework {
             baseName = "Shared"
